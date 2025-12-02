141cd46be94a1c91cf5a2d6d1d7db1efde10a6d0
fabrizio <uksturmanboy@gmail.com>
2025-12-02 19:48:23 -0400

    all functions for conversion text->node implemented

--- src/functions.py
+++ src/functions.py
@@ -16,7 +16,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_list.append(node)
             continue
         if delimiter not in node.text:
-            raise Exception(f"delimiter->{delimiter} not found in text")
+            #raise Exception(f"delimiter->{delimiter} not found in text")
+            new_list.append(node)
+            continue
         if node.text.count(delimiter) % 2 != 0:
             raise Exception(f"delimiter not closed")
         
@@ -30,9 +32,72 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-
-
+    
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
+    
+def split_nodes_image(old_nodes):
+    new_list = []
+    for node in old_nodes:
+        text = node.text
+        image_sec = extract_markdown_images(text)
+        print(image_sec)
+        
+        if not image_sec and text:
+            new_list.append(node)
+            continue
+        final_text = True
+        while image_sec:
+            split_text = text.split(f"![{image_sec[0][0]}]({image_sec[0][1]})",1)
+            new_list.append(TextNode(split_text[0], node.text_type))
+            new_list.append(TextNode(image_sec[0][0], TextType.IMAGE, image_sec[0][1]))
+            try:
+                text = split_text[1]
+            except IndexError:
+                final_text = False
+                break
+            image_sec = extract_markdown_images(text)
+
+        if final_text and text:
+            new_list.append(TextNode(text, node.text_type))
+    return new_list
+
+def split_nodes_link(old_nodes):
+    new_list = []
+    for node in old_nodes:
+        if node.text_type == TextType.IMAGE:
+            new_list.append(node)
+            continue
+        text = node.text
+        link_sec = extract_markdown_links(text)
+
+        if not link_sec and text:
+            new_list.append(node)
+            continue
+        final_text = True
+        while link_sec:
+            split_text = text.split(f"[{link_sec[0][0]}]({link_sec[0][1]})",1)
+            new_list.append(TextNode(split_text[0], node.text_type))
+            new_list.append(TextNode(link_sec[0][0], TextType.LINK, link_sec[0][1]))
+            try:
+                text = split_text[1]
+            except IndexError:
+                final_text = False
+                break
+            link_sec = extract_markdown_links(text)
+
+        if final_text and text:
+            new_list.append(TextNode(text, node.text_type))
+    return new_list
+
+def text_to_textnodes(text):
+    node = TextNode(text, text_type=TextType.TEXT)
+    nodes = split_nodes_image([node])
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.TEXT)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.TEXT)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.TEXT)
+    return nodes
+
 
 
